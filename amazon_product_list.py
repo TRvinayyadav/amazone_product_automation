bccb794d9e216d7b5a2a08d1fe5d9f0f456325cc
Alok Singh <adb.singh143@gmail.com>
2023-09-29 15:59:53 +0530

    Initial Commit

---
+++ amazon_product_list.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+service_object = Service()
+driver = webdriver.Chrome(service=service_object)
+
+driver.get("https://www.amazon.in/")
+driver.find_element(By.XPATH, "//*[@id='nav-xshop']/a[4]").click()
+driver.sleep(10)
+
+product_list= driver.find_elements(By.XPATH, "//div[@class='_octopus-search-result-card_style_apbSearchResultItem__2-mx4']")
+#
+for product in product_list:
+#     with open('abc.csv','a+') as file:
+#         file.write(product.text)
+
+
+    with open('abc.csv', 'a+' , encoding='utf-8') as file:
+        file.write(product.text + '\n')
+
+driver.quit()
